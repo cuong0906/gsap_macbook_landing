8fa602298dac4476aa4b7409d182265b01b33e71
Mai Chi Cuong <maichicuong@Mais-MacBook-Air.local>
2025-10-13 10:19:40 +0700

    Initial commit

---
+++ macbook_gsap_app/src/constants/index.js
@@ -0,0 +1,7 @@
+const navLinks = [
+    {label: 'Mac', link: '#'},
+    {label: 'iPad', link: '#'},
+    {label: 'iPhone', link: '#'},
+    {label: 'Watch', link: '#'},
+    {label: 'AirPods', link: '#'},
+];
