ac3a8f0b1edea4ea7dafa1b8017e59c622751d23
Mai Chi Cuong <maichicuong@Mais-MacBook-Air.local>
2025-10-16 17:20:38 +0700

    Implement Footer

--- macbook_gsap_app/src/components/Footer.jsx
+++ macbook_gsap_app/src/components/Footer.jsx
@@ -1,7 +1,24 @@
+import { footerLinks } from "../constants"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+        <div className="info">
+            <p>More ways to shop: Find an Apple Store or retailers near you. Or call 911</p>
+            <img src="/logo.svg" alt="Apple Logo" />
+        </div>
+        <hr/>
+        <div className="links">
+            <p>Copyright © 2024 Apple Inc. All rights reserved.</p>
+            <ul>
+                {footerLinks.map(({label , link}) => (
+                    <li key={label}>
+                        <a href={link}>{label}</a>  
+                    </li>  
+                ))}
+            </ul>
+        </div>
+    </footer>
   )
 }
 
